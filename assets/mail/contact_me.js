5111928f37a4bd2b1fb96bcf96fcc1db8904b46a
wordpresshizmetleri <selam@wolinka.com>
2021-04-30 17:53:39 +0300

    contact

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -39,7 +39,7 @@ $(function () {
                         )
                         .append("</button>");
                     $("#success > .alert-success").append(
-                        "<strong>Mesajınız gönderildi. Teşekkürler </strong>"
+                        "<strong>Your message has been sent. </strong>"
                     );
                     $("#success > .alert-success").append("</div>");
                     //clear all fields
@@ -55,9 +55,9 @@ $(function () {
                         .append("</button>");
                     $("#success > .alert-danger").append(
                         $("<strong>").text(
-                            "Üzgünüm " +
+                            "Sorry " +
                                 firstName +
-                                ", posta sunucum yanıt vermiyor gibi görünüyor. Lütfen daha sonra tekrar deneyiniz!"
+                                ", it seems that my mail server is not responding. Please try again later!"
                         )
                     );
                     $("#success > .alert-danger").append("</div>");
